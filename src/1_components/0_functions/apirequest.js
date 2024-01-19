fb287a248fd6c15d5722adaeb736b057684f2068
juliokozarewicz <julioerk@outlook.com>
2024-01-19 12:00:35 -0300

    [ build ] Content inside three frame.

--- src/1_components/0_functions/apirequest.js
+++ src/1_components/0_functions/apirequest.js
@@ -2,13 +2,13 @@ import { API_KEY } from '@env';
 
 
 export async function getDataWeather () {
-    const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=*Curitiba&units=metric&units=metric&appid=${API_KEY}`);
+    const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Curitiba&units=metric&units=metric&appid=${API_KEY}`);
     const result = await request.json();
     return result;
 };
 
 export async function getDataForecast () {
-    const requestF = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Curitiba&cnt=8&units=metric&appid=0f44520dea6aec7c56b0ebee83bb0e4b`);
+    const requestF = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=*Curitiba&cnt=8&units=metric&appid=0f44520dea6aec7c56b0ebee83bb0e4b`);
     const resultF = await requestF.json();
     return resultF;
 };
