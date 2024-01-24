e975b033ccd8e7a52abdd19b8830b07b2ec7b254
JULIO KOZAREWICZ <julioerk@outlook.com>
2024-01-24 18:49:36 -0300

    [ Build ] Background by hours day night ok

--- src/1_components/frameone.js
+++ src/1_components/frameone.js
@@ -21,89 +21,113 @@ export function FrameOne(props) {
 
             {
 
-                props.connection 
+                props.connection
                 
                 ?
+
+                    props.data
+
+                    ?
                 
+                        <View style={frameoneStyle.frameoneall}>
+
+                            <View style={frameoneStyle.temperature}>
+                                <View style={frameoneStyle.backgr} ></View>
+                                <View style={frameoneStyle.frametxt1} >
+
+                                    <Text
+                                        style={frameoneStyle.txtblack1}
+                                        numberOfLines={1}
+                                        ellipsizeMode="tail"
+                                    >
+                                        now [ min: { Math.floor(props.data.main.temp_min) }° | max: {Math.floor(props.data.main.temp_max) }° ]
+                                    </Text>
+
+                                    <Text
+                                        style={frameoneStyle.txtpurple}
+                                        numberOfLines={1}
+                                        ellipsizeMode="tail"
+                                    >
+                                        { Math.ceil(props.data.main.temp) }°
+                                    </Text>
+
+                                    <Text
+                                        style={frameoneStyle.txtblack2}
+                                        numberOfLines={1}
+                                        ellipsizeMode="tail"
+                                    >
+                                        fells like: {Math.ceil(props.data.main.feels_like)}°
+                                    </Text>
+
+                                </View>
+                            </View>
+
+                            <View style={frameoneStyle.city}>
+                                <View style={frameoneStyle.backgr2} ></View>
+                                <View style={frameoneStyle.frametxt2} >
+
+                                    <Text
+                                        style={frameoneStyle.txtwhite1}
+                                        numberOfLines={1}
+                                        ellipsizeMode="tail"
+                                    >
+                                        { props.data.name } ({ props.data.sys.country })
+                                    </Text>
+
+                                    <Image source={{ uri: `http://openweathermap.org/img/wn/${props.data.weather[0].icon}@4x.png` }} style={frameoneStyle.imgtemp} />
+
+                                    <Text
+                                        style={frameoneStyle.txtwhite2}
+                                        numberOfLines={2}
+                                        ellipsizeMode="tail"
+                                    >
+                                        { props.data.weather[0].description }
+                                    </Text>
+
+                                </View>
+                            </View>
+
+                        </View>
+
+                    :
+
+                        <View style={frameoneStyle.frameoneall}>
+
+                            <View style={frameoneStyle.temperature}>
+                                <View style={frameoneStyle.backgr} ></View>
+                                <View style={frameoneStyle.frametxt1erro} >
+                                    <Image source={require('./3_img/noconnection.png')} style={frameoneStyle.noconnection} />
+                                </View>
+                            </View>
+
+                            <View style={frameoneStyle.city}>
+                                <View style={frameoneStyle.backgr2} ></View>
+                                <View style={frameoneStyle.frametxt2} >
+                                <Image source={require('./3_img/noconnectionblue.png')} style={frameoneStyle.noconnection} />
+                                </View>
+                            </View>
+
+                        </View>
+                
+                :
+
                     <View style={frameoneStyle.frameoneall}>
 
                         <View style={frameoneStyle.temperature}>
                             <View style={frameoneStyle.backgr} ></View>
-                            <View style={frameoneStyle.frametxt1} >
-
-                                <Text
-                                    style={frameoneStyle.txtblack1}
-                                    numberOfLines={1}
-                                    ellipsizeMode="tail"
-                                >
-                                    now [ min: { Math.floor(props.data.main.temp_min) }° | max: {Math.floor(props.data.main.temp_max) }° ]
-                                </Text>
-
-                                <Text
-                                    style={frameoneStyle.txtpurple}
-                                    numberOfLines={1}
-                                    ellipsizeMode="tail"
-                                >
-                                    { Math.ceil(props.data.main.temp) }°
-                                </Text>
-
-                                <Text
-                                    style={frameoneStyle.txtblack2}
-                                    numberOfLines={1}
-                                    ellipsizeMode="tail"
-                                >
-                                    fells like: {Math.ceil(props.data.main.feels_like)}°
-                                </Text>
-
+                            <View style={frameoneStyle.frametxt1erro} >
+                                <Image source={require('./3_img/noconnection.png')} style={frameoneStyle.noconnection} />
                             </View>
                         </View>
 
                         <View style={frameoneStyle.city}>
                             <View style={frameoneStyle.backgr2} ></View>
                             <View style={frameoneStyle.frametxt2} >
-
-                                <Text
-                                    style={frameoneStyle.txtwhite1}
-                                    numberOfLines={1}
-                                    ellipsizeMode="tail"
-                                >
-                                    { props.data.name } ({ props.data.sys.country })
-                                </Text>
-
-                                <Image source={{ uri: `http://openweathermap.org/img/wn/${props.data.weather[0].icon}@4x.png` }} style={frameoneStyle.imgtemp} />
-
-                                <Text
-                                    style={frameoneStyle.txtwhite2}
-                                    numberOfLines={2}
-                                    ellipsizeMode="tail"
-                                >
-                                    { props.data.weather[0].description }
-                                </Text>
-
+                            <Image source={require('./3_img/noconnectionblue.png')} style={frameoneStyle.noconnection} />
                             </View>
                         </View>
 
                     </View>
-                
-                :
-
-                <View style={frameoneStyle.frameoneall}>
-
-                <View style={frameoneStyle.temperature}>
-                    <View style={frameoneStyle.backgr} ></View>
-                    <View style={frameoneStyle.frametxt1erro} >
-                        <Image source={require('./3_img/noconnection.png')} style={frameoneStyle.noconnection} />
-                    </View>
-                </View>
-
-                <View style={frameoneStyle.city}>
-                    <View style={frameoneStyle.backgr2} ></View>
-                    <View style={frameoneStyle.frametxt2} >
-                    <Image source={require('./3_img/noconnectionblue.png')} style={frameoneStyle.noconnection} />
-                    </View>
-                </View>
-
-            </View>
 
             }
        
--- src/1_components/frametwo.js
+++ src/1_components/frametwo.js
@@ -36,47 +36,58 @@ export function FrameTwo(props) {
                 props.connection 
                 
                 ?
+
+                    props.data
+
+                    ?
                 
+                        <View style={frametwoStyle.frametwoall}>
+                            <View style={frametwoStyle.backgr} ></View>
+
+                            <View style={frametwoStyle.leftside}>
+
+                                <View style={frametwoStyle.linetext}>
+                                    <Text style={frametwoStyle.textpurple}>pressure:</Text>
+                                    <Text style={frametwoStyle.textblack}>  {props.data.main.pressure} hPa</Text>
+                                </View>
+
+                                <View style={frametwoStyle.linetext}>
+                                    <Text style={frametwoStyle.textpurple}>humidity:</Text>
+                                    <Text style={frametwoStyle.textblack}>  {props.data.main.humidity} %</Text>
+                                </View>
+
+                                <View style={frametwoStyle.linetext}>
+                                    <Text style={frametwoStyle.textpurple}>sunrise:</Text>
+                                    <Text style={frametwoStyle.textblack}>  {getdateformated(props.data.sys.sunrise)}</Text>
+                                </View>
+
+                            </View>
+        
+                            <View style={frametwoStyle.rightside}>
+
+                                <View style={frametwoStyle.linetext}>
+                                    <Text style={frametwoStyle.textpurple}>visibility:</Text>
+                                    <Text style={frametwoStyle.textblack}>  {props.data.visibility} m</Text>
+                                </View>
+
+                                <View style={frametwoStyle.linetext}>
+                                    <Text style={frametwoStyle.textpurple}>wind:</Text>
+                                    <Text style={frametwoStyle.textblack}>  {(props.data.wind.speed * 3.6).toFixed(0)} km/h | {props.data.wind.deg}°</Text>
+                                </View>
+
+                                <View style={frametwoStyle.linetext}>
+                                    <Text style={frametwoStyle.textpurple}>sunset:</Text>
+                                    <Text style={frametwoStyle.textblack}>  {getdateformated(props.data.sys.sunset)}</Text>
+                                </View>
+
+                            </View>
+                        </View>
+
+                    :
+
                     <View style={frametwoStyle.frametwoall}>
                         <View style={frametwoStyle.backgr} ></View>
-
-                        <View style={frametwoStyle.leftside}>
-
-                            <View style={frametwoStyle.linetext}>
-                                <Text style={frametwoStyle.textpurple}>pressure:</Text>
-                                <Text style={frametwoStyle.textblack}>  {props.data.main.pressure} hPa</Text>
-                            </View>
-
-                            <View style={frametwoStyle.linetext}>
-                                <Text style={frametwoStyle.textpurple}>humidity:</Text>
-                                <Text style={frametwoStyle.textblack}>  {props.data.main.humidity} %</Text>
-                            </View>
-
-                            <View style={frametwoStyle.linetext}>
-                                <Text style={frametwoStyle.textpurple}>sunrise:</Text>
-                                <Text style={frametwoStyle.textblack}>  {getdateformated(props.data.sys.sunrise)}</Text>
-                            </View>
-
-                        </View>
-    
-                        <View style={frametwoStyle.rightside}>
-
-                            <View style={frametwoStyle.linetext}>
-                                <Text style={frametwoStyle.textpurple}>visibility:</Text>
-                                <Text style={frametwoStyle.textblack}>  {props.data.visibility} m</Text>
-                            </View>
-
-                            <View style={frametwoStyle.linetext}>
-                                <Text style={frametwoStyle.textpurple}>wind:</Text>
-                                <Text style={frametwoStyle.textblack}>  {(props.data.wind.speed * 3.6).toFixed(0)} km/h | {props.data.wind.deg}°</Text>
-                            </View>
-
-                            <View style={frametwoStyle.linetext}>
-                                <Text style={frametwoStyle.textpurple}>sunset:</Text>
-                                <Text style={frametwoStyle.textblack}>  {getdateformated(props.data.sys.sunset)}</Text>
-                            </View>
-
-                        </View>
+                        <Image source={require('./3_img/noconnection.png')} style={frametwoStyle.noconnection} />
                     </View>
                 
                 :
