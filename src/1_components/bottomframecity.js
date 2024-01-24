034eef787299b837749d12c4fb0092df5bd57200
juliokozarewicz <julioerk@outlook.com>
2024-01-24 12:12:38 -0300

    [ Edit ] Add city.

--- src/1_components/bottomframecity.js
+++ src/1_components/bottomframecity.js
@@ -22,104 +22,20 @@ export function BottomFrameCity(props) {
         <>
 
             {
-
-                props.connection 
+                props.menuActivate
 
                 ?
 
-                    props.menuActivate
+                    <View style={bottomframecityStyle.framebottomall} >
+                        <View style={bottomframecityStyle.allcontent} >
+                            <View style={bottomframecityStyle.backgrdwhi} ></View>
 
-                    ?
-
-                        <View style={bottomframecityStyle.framebottomall} >
-                            <View style={bottomframecityStyle.allcontent} >
-                                <View style={bottomframecityStyle.backgrdwhi} ></View>
-
-                                <ScrollView
-                                    showsHorizontalScrollIndicator={false}
-                                    horizontal
-                                    style={bottomframecityStyle.scrollsts}
-                                >
-                                    <View style={bottomframecityStyle.insidescroll} >
-                                        <TouchableOpacity onPress={() => props.setInsertCity(true)} >
-                                            <View style={bottomframecityStyle.city2} >
-                                                <View style={bottomframecityStyle.backgcity} ></View>
-                                                <View style={bottomframecityStyle.addcitiadd} ></View>
-                                                <View style={bottomframecityStyle.addcitiadd2} ></View>
-                                                <Text style={bottomframecityStyle.addcitytxt} >Add City</Text>
-                                            </View>
-                                        </TouchableOpacity>
-
-                                        {
-
-                                            props.getcity.map((item, index) => {
-
-                                                return (
-
-                                                    <TouchableWithoutFeedback key={index} onPress={() => console.log('*** CLICK ***')} >
-                                                        <View style={bottomframecityStyle.city} >
-
-                                                            <View style={bottomframecityStyle.backgcity} ></View>
-
-                                                            <TouchableOpacity
-                                                                style={bottomframecityStyle.btndel}
-                                                                onPress={ () => {
-                                                                    props.deleteItemFromDatabase(item.id);
-                                                                    props.reloadDB();
-                                                                }}
-                                                            >
-                                                                <Image source={require('./3_img/deleteicon.png')} style={bottomframecityStyle.deleteimg} />
-                                                            </TouchableOpacity>
-
-                                                            <Image style={bottomframecityStyle.imgtemp2} source={{ uri: `http://openweathermap.org/img/wn/02d@4x.png` }} />
-
-                                                            <Text
-                                                                numberOfLines={1}
-                                                                ellipsizeMode="tail"
-                                                                style={bottomframecityStyle.txtcity}
-                                                            >
-                                                                {item.city}
-                                                            </Text>
-
-                                                            <Text style={bottomframecityStyle.txttemp} >** °</Text>
-                                                        </View>
-                                                    </TouchableWithoutFeedback>
-
-                                                )
-
-                                                })
-
-                                        }
-
-                                    </View>
-                                </ScrollView>
-
-                            </View>
-                        </View>
-
-                    :
-
-                        <View style={bottomframecityStyle.framebottomall} >
-                            <View style={bottomframecityStyle.allcontent} >
-                                <View style={bottomframecityStyle.backgrdwhi} ></View>
-                            </View>
-                        </View>
-
-                :
-
-                    props.menuActivate
-
-                    ?
-
-                        <View style={bottomframecityStyle.framebottomall} >
-                            <View style={bottomframecityStyle.allcontent} >
-                                <View style={bottomframecityStyle.backgrdwhi} ></View>
-
-                                <ScrollView
-                                    showsHorizontalScrollIndicator={false}
-                                    horizontal
-                                    style={bottomframecityStyle.scrollsts}
-                                >
+                            <ScrollView
+                                showsHorizontalScrollIndicator={false}
+                                horizontal
+                                style={bottomframecityStyle.scrollsts}
+                            >
+                                <View style={bottomframecityStyle.insidescroll} >
                                     <TouchableOpacity onPress={() => props.setInsertCity(true)} >
                                         <View style={bottomframecityStyle.city2} >
                                             <View style={bottomframecityStyle.backgcity} ></View>
@@ -128,25 +44,67 @@ export function BottomFrameCity(props) {
                                             <Text style={bottomframecityStyle.addcitytxt} >Add City</Text>
                                         </View>
                                     </TouchableOpacity>
-                                </ScrollView>
 
+                                    {
 
-                            </View>
+                                        props.getcity.map((item, index) => {
+
+                                            return (
+
+                                                <TouchableWithoutFeedback key={index} onPress={() => console.log('*** CLICK ***')} >
+                                                    <View style={bottomframecityStyle.city} >
+
+                                                        <View style={bottomframecityStyle.backgcity} ></View>
+
+                                                        <TouchableOpacity
+                                                            style={bottomframecityStyle.btndel}
+                                                            onPress={ () => {
+                                                                props.deleteItemFromDatabase(item.id);
+                                                                props.reloadDB();
+                                                            }}
+                                                        >
+                                                            <Image source={require('./3_img/deleteicon.png')} style={bottomframecityStyle.deleteimg} />
+                                                        </TouchableOpacity>
+
+                                                        <Image style={bottomframecityStyle.imgtemp2} source={{ uri: `http://openweathermap.org/img/wn/02d@4x.png` }} />
+
+                                                        <Text
+                                                            numberOfLines={1}
+                                                            ellipsizeMode="tail"
+                                                            style={bottomframecityStyle.txtcity}
+                                                        >
+                                                            {item.city}
+                                                        </Text>
+
+                                                        <Text style={bottomframecityStyle.txttemp} >** °</Text>
+                                                    </View>
+                                                </TouchableWithoutFeedback>
+
+                                            )
+
+                                            })
+
+                                    }
+
+                                </View>
+                            </ScrollView>
+
                         </View>
+                    </View>
 
-                    :
+                :
 
-                        <View style={bottomframecityStyle.framebottomall} >
-                            <View style={bottomframecityStyle.allcontent} >
-                                <View style={bottomframecityStyle.backgrdwhi} ></View>
-                            </View>
+                    <View style={bottomframecityStyle.framebottomall} >
+                        <View style={bottomframecityStyle.allcontent} >
+                            <View style={bottomframecityStyle.backgrdwhi} ></View>
                         </View>
+                    </View>
 
             }
-       
+
         </>
-    
+
     );
-    
+
 };
 // -------------------------------------------------------------------------------------
