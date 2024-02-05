3a57c558192cf69fbae9b593f02ab6d63926e1ea
juliokozarewicz <julioerk@outlook.com>
2024-02-05 10:25:13 -0300

    [ fixed ] Units config per country.

--- src/1_components/0_functions/apirequest.js
+++ src/1_components/0_functions/apirequest.js
@@ -5,14 +5,22 @@ const GET_API_KEY = API_KEY;
 
 const lang = Platform.OS === 'ios' ? NativeModules.SettingsManager.settings.AppleLocale : NativeModules.I18nManager.localeIdentifier;
 
+let units;
+
+if (lang.slice(-2).toLowerCase() === 'us') {
+    units = 'standard';
+} else {
+    units = 'metric';
+}
+
 export async function getDataWeather (city) {
-    const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&units=metric&units=metric&appid=${GET_API_KEY}`);
+    const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&units=${units}&appid=${GET_API_KEY}`);
     const result = await request.json();
     return result;
 };
 
 export async function getDataForecast(city) {
-        const requestF = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=${lang}&cnt=&units=metric&appid=${GET_API_KEY}`);
+        const requestF = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=${lang}&cnt=&units=${units}&appid=${GET_API_KEY}`);
         const resultF = await requestF.json();
         return resultF;
 };
